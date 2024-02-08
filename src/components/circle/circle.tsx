fde8906ea88a6595759c1457d5b177375129b4dd
Mila <107352719+4ebure4ek0@users.noreply.github.com>
2024-02-08 21:09:54 +0300

    fix points on circle

--- src/components/circle/circle.tsx
+++ src/components/circle/circle.tsx
@@ -12,10 +12,10 @@ interface Points{
 
 const  Circle = (props:IProps) => {
     const[showPoint, setShowPoint] = useState(false)
+    const[arrPoints, setArrPoints] = useState([])
     const canvasRef = useRef<HTMLCanvasElement | null>(null)
     const pointRef = useRef(null)
     let r = 260
-    let arrPoints:Array<Points> = []
     useEffect(() => {
         let canvas = canvasRef.current
         canvas.style.width = canvas.offsetWidth + 'px';
@@ -26,36 +26,40 @@ const  Circle = (props:IProps) => {
         ctx.beginPath()
         ctx.arc(canvasRef.current?.width / 2, canvasRef.current?.height / 2, r, 0, 2*Math.PI);
         ctx.stroke()
+        let arr = []
         let x, y;
         for(let i = 0; i < Math.PI * 2; i += Math.PI * 2 / props.points){
             x = canvasRef.current.width / 2 + r * Math.sin(i);
             y = canvasRef.current.height / 2 + r * Math.cos(i);
-            ctx.lineWidth = 1;
-            ctx.strokeStyle = 'rgba(66, 86, 122, 1)'
-            ctx.beginPath()
-            ctx.arc(x, y, 2, 0, 2*Math.PI);
-            ctx.fill()
-            ctx.text
-            ctx.stroke()
-            arrPoints.push({'x': x, 'y': y})
+            // ctx.lineWidth = 1;
+            // ctx.strokeStyle = 'rgba(66, 86, 122, 1)'
+            // ctx.beginPath()
+            // ctx.arc(x, y, 2, 0, 2*Math.PI);
+            // ctx.fill()
+            // ctx.text
+            // ctx.stroke()
+            arr.push({'x': x, 'y': y})
         }
+        setArrPoints([...arr])
     }, [])
-    // useEffect(() => {
-
-    // }, [points])
-    const  handleHover = (e:any) => {
-        let rect = canvasRef.current.getBoundingClientRect()
-        const ctx = canvasRef.current.getContext("2d") as any
-        arrPoints.forEach((point, n) => {
-            if((point.x - (e.pageX - rect.left) <= 5 && point.x - (e.pageX - rect.left) >= -5) && (point.y - (e.pageY - rect.top) <= 5 && point.y - (e.pageY - rect.top) >= -5)){
-                setShowPoint(true)
-            }
-        })
-    }
+    // const  handleHover = (e:any) => {
+    //     let rect = canvasRef.current.getBoundingClientRect()
+    //     const ctx = canvasRef.current.getContext("2d") as any
+    //     arrPoints.forEach((point, n) => {
+    //         if((point.x - (e.pageX - rect.left) <= 5 && point.x - (e.pageX - rect.left) >= -5) && (point.y - (e.pageY - rect.top) <= 5 && point.y - (e.pageY - rect.top) >= -5)){
+    //             setShowPoint(true)
+    //         }
+    //     })
+    // }
+    console.log(arrPoints)
     return(
         <>
-            <canvas className={styles.circle} ref={canvasRef} id='circle' width={530} height={530} onMouseMove={handleHover}></canvas>
-            {showPoint? <div className={styles.point} ref={pointRef}></div> : null}
+            <canvas className={styles.circle} ref={canvasRef} id='circle' width={530} height={530}>
+            </canvas>
+            <div className={styles.cover_circle}>
+                {arrPoints.map((point, n) => <span key={n} className={styles.point} style={{'top': `${point.y}px`, 'left': `${point.x}px`}}>{n+1}</span>)}
+            </div>
+            {/* {showPoint? <div className={styles.point} ref={pointRef}></div> : null} */}
         </>
     )
 }
